d2e913a0f45fb781fd49d035ca64f2dbc779ac00
mikewainaina <michael.wainaina@student.moringaschool.com>
2024-05-25 21:37:32 +0300

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [y for y in num_list if y % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return[sentence +"!" for sentence in sentence_list]
